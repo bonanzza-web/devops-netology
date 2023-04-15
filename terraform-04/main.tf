2d8532456cb8fb0e6ffcfd8c7d6c1bb46f603ae5
Dmitry Ananiev <frukt07@inbox.ru>
2023-04-15 15:22:16 +0300

    delete comments

--- terraform-04/main.tf
+++ terraform-04/main.tf
@@ -16,19 +16,6 @@ provider "yandex" {
 
 
 
-#создаем облачную сеть
-# resource "yandex_vpc_network" "develop" {
-#   name = "develop"
-# }
-
-#создаем подсеть
-# resource "yandex_vpc_subnet" "develop" {
-#   name           = "develop-ru-central1-a"
-#   zone           = "ru-central1-a"
-#   network_id     = yandex_vpc_network.develop.id
-#   v4_cidr_blocks = ["10.0.1.0/24"]
-# }
-
 module "vpc" {
   source = "./vpc"
   vpc_name = "my-vpc"
